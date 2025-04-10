f4e63d7f928bf18ac0372c02d230eb1d1dc70e70
PokeFred <Cedric.paelmke@gmx.de>
2025-04-11 00:43:25 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,62 @@
-<script lang="ts">
-    import { onMount } from "svelte"
+<svelte:head>
+    <script src="https://cdn.tailwindcss.com"></script>
+</svelte:head>
 
-    onMount(async (): Promise<void> => {})
+<script lang="ts">
+    type Asset = {
+        image: string,
+        name: string,
+        exchange: string
+    }
+
+    const assets: Asset[] = [
+        { image: "/t.svg", name: "Nvidia", exchange: "Xetra" },
+        { image: "/t.svg", name: "Nvidia", exchange: "Gettex" },
+        { image: "/t.svg", name: "Nvidia", exchange: "LS Exchange" }
+    ]
 </script>
 
-<div>Hello World</div>
+<div class="p-4">
+    <div class="mx-auto w-auto max-w-[512px] h-auto border border-black rounded-xl grid grid-cols-1 gap-y-2 p-4">
+        <div class="mb-3 text-3xl font-bold underline text-center">Step 1: Search the Exchange</div>
+    </div>
+    <div class="mt-3 mx-auto w-auto max-w-[512px] h-auto border border-black rounded-xl grid grid-cols-1 gap-y-2 p-4">
+        <div class="mb-3 text-3xl font-bold underline text-center">Step 2: Choose the Exchange</div>
+        {#each assets as element}
+            <div class="w-full h-auto border border-black rounded-xl flex">
+                <img src="{element.image}" alt="" class="w-16 h-16 rounded-l-xl shrink-0" />
+                <div class="w-full h-16 px-4 py-1">
+                    <div class="w-full h-8 text-xl font-bold flex justify-start items-center">{element.name}</div>
+                    <div class="w-full h-6 text-sm flex justify-start items-center">{element.exchange}</div>
+                </div>
+                <div class="w-fit h-16 rounded-r-xl flex justify-center items-center shrink-0 px-4">
+                    <button class="w-fit h-auto text-white bg-blue-500 rounded-xl cursor-pointer px-4 active:scale-90">Select</button>
+                </div>
+            </div>
+        {/each}
+    </div>
+    <div class="mt-3 mx-auto w-auto max-w-[512px] h-auto border border-black rounded-xl grid grid-cols-1 gap-y-2 p-4">
+        <div class="mb-3 text-3xl font-bold underline text-center">Step 3: Add Custom Data</div>
+        <form>
+            <label>
+                <span>Official Name:</span>
+                <input type="text" value="NVIDIA CORP." class="border border-black rounded-xl px-4" />
+            </label>
+            <br />
+            <label>
+                <span>Name:</span>
+                <input type="text" value="Nvidia" class="border border-black rounded-xl px-4" />
+            </label>
+            <br />
+            <label>
+                <span>ISIN:</span>
+                <input type="text" value="US67066G1040" class="border border-black rounded-xl px-4" />
+            </label>
+            <br />
+            <label>
+                <span>WKN:</span>
+                <input type="text" value="918422" class="border border-black rounded-xl px-4" />
+            </label>
+        </form>
+    </div>
+</div>
--- src/routes/api/one/+server.ts
+++ src/routes/api/one/+server.ts
@@ -2,13 +2,15 @@ import type { RequestHandler } from "./$types"
 import { Builder, Browser, By, until } from "selenium-webdriver"
 import type { WebDriver, WebElement } from "selenium-webdriver"
 import { Options } from "selenium-webdriver/chrome"
+import axios from "axios"
+import type { AxiosResponse } from "axios"
 
 type Exchange = {
     notation: string,
     name: string
 }
 
-async function fetchExchanges(isin: string): Promise<Exchange[]> {
+async function fetchExchanges(identifier: string): Promise<Exchange[]> {
     const options: Options = new Options()
     options.addArguments("--headless")
 
@@ -19,11 +21,11 @@ async function fetchExchanges(isin: string): Promise<Exchange[]> {
 
     let exchanges: Exchange[] = []
     try {
-        let url: string = `https://www.onvista.de/suche/?searchValue=${isin}`
+        let url: string = `https://www.onvista.de/suche/?searchValue=${identifier}`
         await driver.get(url)
 
         url = await driver.getCurrentUrl()
-        url = url.replace("https://www.onvista.de/etf", "https://www.onvista.de/etf/handelsplaetze")
+        url = url.replace("https://www.onvista.de/aktien", "https://www.onvista.de/aktien/handelsplaetze")
         await driver.get(url)
 
         await driver.wait(until.elementsLocated(By.css("select#select-history-market")), 5000)
@@ -50,17 +52,79 @@ async function fetchExchanges(isin: string): Promise<Exchange[]> {
     return exchanges
 }
 
-export const GET: RequestHandler = async (): Promise<Response> => {
-    const isin: string = "IE00B4L5Y983"
-    const exchanges: Exchange[] = await fetchExchanges(isin)
+type A = {
+    type: string,
+    name: string,
+    isin: string,
+    wkn: string,
+    id: string,
+    exchanges: Exchange[]
+}
 
-    const data: string = JSON.stringify(exchanges)
-    return new Response(data, {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Content-Length": data.length.toString(),
-            "Accept-Ranges": "bytes"
+async function fetchData(): Promise<A | null> {
+    try {
+        const response: AxiosResponse = await axios({
+            method: "GET",
+            url: "https://api.onvista.de/api/v1/stocks/ISIN:US67066G1040/snapshot"
+        })
+        const data = response.data
+
+        return {
+            type: data.instrument.entityType,
+            name: data.instrument.name,
+            isin: data.instrument.isin,
+            wkn: data.instrument.wkn,
+            id: data.quote.idInstrument,
+            exchanges: []
         }
-    })
+    } catch (error: unknown) {
+        console.log(error)
+        return null
+    }
+}
+
+export const GET: RequestHandler = async ({ url }): Promise<Response> => {
+    const isin: string | null = url.searchParams.get("isin")
+    const wkn: string | null = url.searchParams.get("wkn")
+
+    let identifier: string | null = null
+    if ((isin !== null) && (isin.length === 12)) identifier = isin
+    else if ((wkn !== null) && (wkn.length === 6)) identifier = wkn
+
+    if (identifier !== null) {
+        const raw: A | null = await fetchData()
+
+        if (raw === null) {
+            const data: string = JSON.stringify({ message: "Error: invalid isin or wkn." })
+            return new Response(data, {
+                status: 400,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": data.length.toString(),
+                    "Accept-Ranges": "bytes"
+                }
+            })
+        }
+        raw.exchanges = await fetchExchanges(identifier)
+
+        const data: string = JSON.stringify(raw)
+        return new Response(data, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": data.length.toString(),
+                "Accept-Ranges": "bytes"
+            }
+        })
+    } else {
+        const data: string = JSON.stringify({ message: "Error: invalid isin or wkn." })
+        return new Response(data, {
+            status: 400,
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": data.length.toString(),
+                "Accept-Ranges": "bytes"
+            }
+        })
+    }
 }
